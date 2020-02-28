ae38243ec663b01348c1389e8a120c25fe77f070
Alexander B <addlesssugar@gmail.com>
2020-02-29 00:38:24 +0300

    added template 'first'

---
+++ src/Final_tasks/first.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import rospy
+from sensor_msgs.msg import CompressedImage
+from numpy import *
+import threading
+import traceback
+import time
+import sys
+import tf.transformations as tftr
+import math
+
+from geometry_msgs.msg import Pose, Point, Vector3, Twist
+from sensor_msgs.msg import CameraInfo, Imu
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Empty
+from tello_driver.msg import TelloStatus
+
+class DroneController:
+
+    def __init__(self):
+	    #ROS PUB
+        self.cmd_vel_pub = rospy.Publisher('/tello/cmd_vel', Twist, queue_size=1, latch=True)
+        self.emergency_pub = rospy.Publisher('/tello/emergency', Empty, queue_size=1, latch=True)
+        self.fast_mode_pub = rospy.Publisher('/tello/fast_mode', Empty, queue_size=1, latch=True)
+        self.flattrim_pub = rospy.Publisher('/tello/flattrim', Empty, queue_size=1, latch=True)
+        self.land_pub = rospy.Publisher('/tello/land', Empty, queue_size=1, latch=True)
+        self.takeoff_pub = rospy.Publisher('/tello/takeoff', Empty, queue_size=1, latch=True)
+        self.manual_takeoff_pub = rospy.Publisher('/tello/manual_takeoff', Empty, queue_size=1, latch=True)
+
+        #ROS SUB
+        self.odom_sub = rospy.Subscriber('/tello/odom', Odometry, self.odom_callback)
+        self.imu_sub = rospy.Subscriber('/tello/imu', Imu, self.imu_callback)
+        self.status_sub = rospy.Subscriber('/tello/status', TelloStatus, self.status_callback)
+
+        #DRONE STATE
+        self.odom = Odometry()
+        self.imu = Imu()
+        self.status = TelloStatus()
+
+        #CALL START METHOD
+        self.start_scenario()
+
+    def odom_callback(self, msg):
+        lock.acquire()
+        self.odom = msg #SAVE FRESH ODOM
+        lock.release()
+
+    def imu_callback(self, msg):
+        lock.acquire()
+        self.imu = msg #SAVE FRESH IMU
+        lock.release()
+
+    def status_callback(self, msg):
+        lock.acquire()
+        self.status = msg #SAVE FRESH STATUS
+        lock.release()
+
+    def stop(self):
+        """ Reset the robot """
+        msg = Empty()
+        self.land_pub.publish(msg)
+
+        self.odom_sub.unregister()
+        self.imu_sub.unregister()
+        self.status_sub.unregister()
+
+    def start_scenario():
+        print("Hello There, mr Robot!")
+
+        # WRITE YOUR CODE HERE
+
+        self.stop()
