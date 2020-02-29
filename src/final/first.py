78a2ecfb89c51316f294db94dca0efe79641ca17
Alexander B <addlesssugar@gmail.com>
2020-02-29 15:57:01 +0300

    upd

--- src/final/first.py
+++ src/final/first.py
@@ -9,7 +9,7 @@ import time
 import sys
 import tf.transformations as tftr
 import math
-#from pid import *
+from pid import *
 
 from sensor_msgs.msg import CompressedImage
 from geometry_msgs.msg import Pose, Point, Vector3, Twist
@@ -49,15 +49,22 @@ class DroneController:
         self.state_lin_vel = Vector3()
         self.state_ang_vel = Vector3()
 
-        #CALL START METHOD
+        #ERRORS
+        self.x_error = 0.0
+        self.y_error = 0.0
+        self.z_error = 0.0
+        self.theta_error = 0.0
+        self.dist = 0.0
+
+        self.precision = 0.10
 
     def odom_callback(self, msg):
         lock.acquire()
         q = msg.pose.pose.orientation
         roll, pitch, yaw = tftr.euler_from_quaternion((q.x, q.y, q.z, q.w))
-        self.drone_state_position = msg.pose.pose.position
-        self.drone_state_position.y *= -1
-        self.drone_state_position.z *= -1
+        self.state_position = msg.pose.pose.position
+        self.state_position.y *= -1
+        self.state_position.z *= -1
 
         self.state_orientation = Vector3(roll, -pitch, -yaw)
         self.state_lin_vel = msg.twist.twist.linear
@@ -98,21 +105,31 @@ class DroneController:
         print("Emergency stop")
         self.emergency_pub.publish(msg)
 
-    def send_velocity(self):
+    def send_velocity(self, Vx, Vy, Vz, Wz):
         velocity = Twist()
         velocity.linear.x = Vx
-        velocity.linear.y = Vy
+        velocity.linear.y = -Vy
+        velocity.linear.z = -Vz
         velocity.angular.z = Wz
-        self.robotino_cmd_vel_pub.publish(velocity)
+        self.cmd_vel_pub.publish(velocity)
+
     #def theta_stabilization(self)
     #    if (self.state_orientation.z > math.pi):
+
+    def get_error(self, goal_x, goal_y, goal_z, goal_theta):
+        self.x_erorr = goal_x - self.state_position.x
+        self.y_erorr = goal_y - self.state_position.y
+        self.z_error = goal_z - self.state_position.z
+        self.theta_erorr = goal_theta - self.state_orientation.z
+        self.dist = sqrt(self.x_error ** 2 + self.y_error ** 2)
 
     def stabilization(self):
         velocity = Twist()
         velocity.linear.x = 0
         velocity.linear.y = 0
         velocity.linear.z = 0
-        self.robotino_cmd_vel_pub.publish(velocity)
+        velocity.angular.z = 0
+        self.cmd_vel_pub.publish(velocity)
 
     # def update():
     #     #main loop
@@ -122,53 +139,79 @@ class DroneController:
 if __name__ == '__main__':
     rospy.init_node("main_solve_node")
     drone = DroneController()
-    PID_Z = PID(0.8, 0, 0, 0.02)
+    PID_Z = PID(1.15, 0.4, 0.0, 0.5)
     dt = 0.05
     flag_takeoff = False
 
-    h = 1.0
+    h = 1.5
     dh = 0.5
-    theta = 0.5
+    theta = 90 * pi / 180
 
     state = 0
+    fix_ang = 0
 
     r = rospy.Rate(1/dt)
     while not rospy.is_shutdown():
-            drone.update()
-            try:
-                #take off
-                if state == 0:
-                    if flag_takeoff == False:
-                        drone.takeoff()
-                        flag_takeoff = True
-                        rospy.sleep(4)
-                    #make sure state pos z ++
-                    if #erorr > 0.05
-                        Vz = PID_Z.updatePidControl(drone.state_position.z, h, dt) #z
-                        continue
+        r.sleep()
+        drone.get_error(0.0, 0.0, h, theta - fix_ang)
+        print("Err: " + str(round(drone.x_error, 4)) + " " + str(round(drone.y_error, 4)) + " " + str(round(drone.z_error, 4)) + " " + str(round(drone.theta_error, 4)))
 
+        try:
+            #take off
+            if state == 0:
+                if flag_takeoff == False:
+                    drone.takeoff()
+                    print("Takeoff")
+                    flag_takeoff = True
+                #make sure state pos z ++
+                if abs(drone.z_error) > drone.precision:
+                    Vz = PID_Z.updatePidControl(h, drone.state_position.z, dt) #z
+                    print("Control V: " + str(Vz))
+                    drone.send_velocity(0.0, 0.0, Vz, 0.0)
+                    continue
+                else:
+                    print("THE GOAL 0 IS REACHED")
+                    drone.stabilization()
+                    print("SLEEPING")
+                    rospy.sleep(5)
+                    print("STOP SLEEPING")
+                    # drone.land()
+                    fix_ang = drone.state_orientation.z
+                    state = 1
 
-
-                    
-                #change theta
-                elif state == 1:
-                    PID.updatePidControl() #theta
-                #change height
-                elif state == 2:
-                    PID.updatePidControl(drone.state_position.z, , dt) #z
-                #land
-                elif state == 3:
+            #change theta
+            if state == 1:
+                Kr = 0.2
+                theta_err = drone.theta_erorr
+                print("Theta err: " + str(theta_err) + " Theta: " + str(drone.state_orientation.z))
+                # theta_err += 2*math.pi if drone.state_orientation.z > math.pi else 0
+                if abs(theta_err) > 10 * pi / 180:
+                    drone.send_velocity(0.0, 0.0, 0.0, Kr * theta_err)
+                    continue
+                else:
+                    print("THE GOAL 1 IS REACHED")
+                    drone.stabilization()
+                    print("SLEEPING")
+                    rospy.sleep(5)
+                    print("STOP SLEEPING")
                     drone.land()
-                state += 1
-            except rospy.ROSInterruptException as e:
-                drone.emergency_stop()
-                drone.stop()
-                del drone
-                print('End')
-            r.sleep()
-    drone.takeoff()
-    rospy.sleep(10)
-    drone.land()
+                    state = 2
+
+            # #change height
+            # elif state == 2:
+            #     PID.updatePidControl(drone.state_position.z, , dt) #z
+            # #land
+            # elif state == 3:
+            #     drone.land()
+        except rospy.ROSInterruptException as e:
+            drone.emergency_stop()
+            drone.stop()
+            del drone
+            print('End')
+
+    # drone.takeoff()
+    # rospy.sleep(10)
+    # drone.land()
     drone.stop()
     del drone
     print('End')
--- src/final/pid.py
+++ src/final/pid.py
@@ -8,9 +8,6 @@ import time
 class PID(object):
 
     def __init__(self, coeff_p, coeff_i, coeff_d, anti_windup):
-
-        self.goal_pose = goal_pose
-        self.current_pose = current_pose
         # p = 0.8 i = 0.0001 d = 0.003
         self.coeff_p = coeff_p
         self.coeff_i = coeff_i
@@ -40,7 +37,7 @@ class PID(object):
 
         self.control = self.coeff_p * self.pose_error + self.coeff_i * self.integral_error + self.coeff_d * self.derivative_error
 
-        return control
+        return self.control
 
     def relu(self, input, value):
         if input > value:
